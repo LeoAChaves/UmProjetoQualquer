99c6942b3693da36218206ff209c693f20aa9c5f
LeoAChaves <leochaves2121@gmail.com>
2022-02-24 18:15:32 -0300

    Atualizando manualmente os itens do dia

---
+++ src/js/dayli.js
@@ -0,0 +1,20 @@
+const daily = [
+    "22-02" = {
+        "quote":"If you think you're so strong that you can do anything, behold areally tough challenge: say no to an opportunity and take a nap",
+        "pokemon":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/222.png",
+        "apod":"https://apod.nasa.gov/apod/image/2202/Quasar_EsoKormesser_1080.jpg",
+        "minecraft":""
+    },
+    "23-02" = {
+        "quote":'"Give me six hours to chop down a tree and I will spend the first four sharpening the axe." - Abraham Lincoln',
+        "pokemon":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/232.png",
+        "apod":"",
+        "minecraft":"Bottle_of_Enchanting.gif"
+    },
+    "24-02" = {
+        "quote":"",
+        "pokemon":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/242.png",
+        "apod":"https://apod.nasa.gov/apod/image/2202/albireoSpectrum1024.jpg",
+        "minecraft":"Enchanted_Book.gif"
+    },
+]
